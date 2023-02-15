b5cdcd92192f68236eb579bf8d7c298bc9487eb7
Brandon Nolan <brndalx@gmail.com>
2023-02-15 17:03:39 +0200

    feat: login router with autentication and jwt return

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { UserModel, validateJoi } = require("../models/userModel");
+const {
+  UserModel,
+  validateJoi,
+  validateLogin,
+} = require("../models/userModel");
 const bcrypt = require("bcrypt");
 
 //Function to generate random amount of starts when JSON response will be returned to the user;
@@ -12,6 +16,7 @@ router.get("/", async (req, res) => {
   res.json({ msg: "Users endpoint" });
 });
 
+//Retrieving a user's information based on the token that is sent in the request header.
 router.get("/userInfo", async (req, res) => {
   //req.header is used to send token in the header as more secure way to operate ith sesitive info
   //x-api-key - header name whic will be checked if he does provided valid token
@@ -64,6 +69,31 @@ router.post("/", async (req, res) => {
       });
     }
     // To handle default error that may thrown if 11000 hasnt occured
+    console.log(err);
+    res.status(502).json({ err });
+  }
+});
+// Authenticating a user by verifying their email and password and returning a JSON web token if the authentication is successful.
+router.post("/login", async (req, res) => {
+  let validBody = validateLogin(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error);
+  }
+  try {
+    // Checks if the email sent to body even exists in the database
+    let user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ msg: "Email not found / user dont exist" });
+    }
+    // Checks if the encrypted password in the database matches the password in body sended, if not throws error
+    let validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (validPassword) {
+      return res.status(401).json({ msg: "Password you're entered is wrong" });
+    }
+    // The user will be sent a token that will allow him to be in areas that require permission;
+    let newToken = createToken(user._id);
+    res.json({ token: newToken });
+  } catch (err) {
     console.log(err);
     res.status(502).json({ err });
   }
