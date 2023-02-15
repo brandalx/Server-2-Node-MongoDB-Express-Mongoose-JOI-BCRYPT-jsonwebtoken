75446aa8bbf23601bd2a865dd4a3228cc8f0a65d
Brandon Nolan <brndalx@gmail.com>
2023-02-15 03:06:08 +0200

    feat: rotes for each category created

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,5 +1,11 @@
 const indexRoute = require("./index");
+const usersRoute = require("./users");
+const drinksRoute = require("./drinks");
+const categoriesRoute = require("./categories");
 
 exports.routesInit = (app) => {
   app.use("/", indexRoute);
+  app.use("/users", usersRoute);
+  app.use("/drinks", drinksRoute);
+  app.use("/categories", categoriesRoute);
 };
