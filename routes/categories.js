087fd7aa6973a776c81e918b3d525cae048c205d
Brandon Nolan <brndalx@gmail.com>
2023-02-15 12:25:00 +0200

    feat: comments added

--- routes/categories.js
+++ routes/categories.js
@@ -3,15 +3,22 @@ const router = express.Router();
 const { CategoryModel } = require("../models/categoriesModel");
 
 router.get("/", async (req, res) => {
+  //variable to use per page
   let perPage = 5;
+  //variable to list the page
   let page = req.query.page - 1 || 0;
+  //variable to sort if not will use an mongo _id
   let sort = req.query.sort || "_id";
+  //variable to sort by acs or desc order
   let desc = req.query.desc == "yes" ? 1 : -1;
 
   try {
     let data = await CategoryModel.find({})
-      .limit(5)
+      //makes display limit per page
+      .limit(perPage)
+      //helps to make two variables works together in order to achieve page listing
       .skip(page * perPage)
+      //makes an possible to sort by choosen option
       .sort({ [sort]: desc });
     res.json(data);
   } catch (err) {
