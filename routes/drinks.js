0817697827c333531a99cce567a2f82c30d9aa65
Brandon Nolan <brndalx@gmail.com>
2023-02-15 14:02:44 +0200

    feat: GET request for drinks route

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const { DrinkModel, validateJoi } = require("../models/drinksModel");
+
+//In POSTMAN  change: request type to to GET, choose Body ===> row
+
+// GET request handle
+//Query example: http://localhost:3002/drinks/?page=1&sort=name&desc=yes
 
 router.get("/", async (req, res) => {
-  res.json({ msg: "Drinks router works correctly" });
+  let perPage = 5;
+  // Variable to list the page
+  let page = req.query.page - 1 || 0;
+  // Variable to sort if not will use an mongo _id
+  let sort = req.query.sort || "_id";
+  // Variable to sort by acs or desc order
+  let desc = req.query.desc == "yes" ? 1 : -1;
+  try {
+    let data = await DrinkModel.find({})
+      //makes display limit per page
+      .limit(perPage)
+      //helps to make two variables works together in order to achieve page listing
+      .skip(page * perPage)
+      //makes an possible to sort by choosen option
+      .sort({ [sort]: desc });
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(502).json({ err });
+  }
 });
 
+//exports whole drinks route to config routes
 module.exports = router;
