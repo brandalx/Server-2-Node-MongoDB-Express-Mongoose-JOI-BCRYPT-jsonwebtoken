bcb5be110dbddebe24b254faf4e5a8110153d8ed
Brandon Nolan <brndalx@gmail.com>
2023-02-15 16:16:37 +0200

    feat: POST route for new user with hashed password using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const { UserModel } = require("../models/userModel");
+const { UserModel, validateJoi } = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+//Function to generate random amount of starts when JSON response will be returned to the user;
+const randomStars = "*".repeat(Math.floor(Math.random() * 10) + 1);
 
 //To someone will have not direct access to the users data base it will send by / to default users route endpoint
 router.get("/", async (req, res) => {
@@ -24,14 +28,44 @@ router.get("/userInfo", async (req, res) => {
     let data = await UserModel.findOne(
       //Searches the user record that matches the _id extracted from the decoded token
       { _id: decodeToken._id },
-      // Exclude the user's password from the retrieved data
-      { password: 0 }
+      // Exclude the user's password from the retrieved data by creating random amount of "*"
+      { password: randomStars }
     );
     res.json(data);
   } catch (err) {
     //Throws error if anything above in try block was wrong
     console.log(err);
     res.status(401).json({ err: "Token invalid or expired" });
+  }
+});
+
+//POST Route for creating a new user with a hashed password by using bcrypt
+router.post("/", async (req, res) => {
+  let validBody = validateJoi.apply(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let user = new UserModel(req.body);
+    //Encrypting the password property so that it is stored encrypted in the database; salt value of 10
+    user.password = await bcrypt.hash(user.password, 10);
+    //Then save to Mongo DB occures
+    await user.save();
+    // Exclude the user's password from the retrieved data by creating random amount of "*"
+    user.password = randomStars;
+    //JSON wth user data returns to the user
+    res.json(user);
+  } catch (err) {
+    //If a user with the same email already exists in the system the route throwns error
+    if ((err.code = 11000)) {
+      return res.status(400).json({
+        msg: "This email is already exist in our system, please try log in again",
+        code: 11000,
+      });
+    }
+    // To handle default error that may thrown if 11000 hasnt occured
+    console.log(err);
+    res.status(502).json({ err });
   }
 });
 
